5ae5a485a82393645504333e3b3f1e27bb75b9cb
B.N <19542357+BNSC1@users.noreply.github.com>
2022-10-03 16:54:25 +0800

    - Update: shows current animal name on action bar

--- app/src/main/java/com/bn/taipeizoo/ui/TaipeiZooAnimalDetailFragment.kt
+++ app/src/main/java/com/bn/taipeizoo/ui/TaipeiZooAnimalDetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.bn.taipeizoo.R
@@ -30,6 +31,7 @@ class TaipeiZooAnimalDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        (requireActivity() as AppCompatActivity).supportActionBar?.title = selectedAnimal.chineseName
         with(binding) {
             Glide.with(this@TaipeiZooAnimalDetailFragment).load(selectedAnimal.pic1Url)
                 .into(animalImage)
